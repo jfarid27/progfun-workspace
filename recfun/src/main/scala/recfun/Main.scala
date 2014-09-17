df3731d45b4af42d1899fc031cb9fa3ebe2b99f2
jfarid27 <jrab227@gmail.com>
2014-09-16 22:13:12 -0400

    More additions

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,53 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 & r == 0) 1
+    else {
+      if (c == 0 || c == r) 1
+      else {
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
+      }
+    }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+
+  def balanceRec(balance: Int, chars: List[Char]): Boolean =
+
+    if (chars.isEmpty) {
+      if (balance == 0) {
+        return true
+      } else {
+        return false
+      }
+    } else if (balance < 0) {
+      return false
+    }
+    else {
+      if (chars.head.toString() == "(") {
+        return balanceRec(balance + 1, chars.tail)
+      } else if (chars.head.toString() == ")") {
+        return balanceRec(balance - 1, chars.tail)
+      } else {
+        return balanceRec(balance, chars.tail)
+      }
+    }
+
+  def balance(chars: List[Char]): Boolean = balanceRec(0, chars)
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    for {
+      coin <- coins
+    } yield countChange(money, List(coin))
+  }
+
+
+
 }
---
+++ recfun/src/main/scala/recfun/tester.sc
@@ -0,0 +1,4 @@
+import recfun.Main._
+object tester {
+  val sums = countChange(2, List(1, 2))
+}
