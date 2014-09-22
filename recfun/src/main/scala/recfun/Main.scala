528a94ce48f3b524f6003dc5e7877c5cd9cdb690
jfarid27 <jrab227@gmail.com>
2014-09-21 20:45:12 -0400

    Cleaned Main file

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -54,12 +54,68 @@ object Main {
    * Exercise 3
    */
 
+  def countChangeRec(money:Int, denominations:List[Int]): Set[List[Int]] = {
+
+    if (denominations.isEmpty){
+      return Set()
+    }
+
+    //If money == denominations.head
+    if (money == denominations.head){
+      //if denominatons.tail.isEmpty
+      if (denominations.tail.isEmpty){
+        //Return set with denomination only
+        return Set(List(denominations.head))
+
+      //else tail isn't empty
+      } else {
+        return Set(List(denominations.head)) ++ countChangeRec(money, denominations.tail)
+        //return 1 + countChangeRec(money, denominations.tail)
+      }
+
+
+    } else if (money > denominations.head) {
+      //build sets with only denominations.head money amount
+      val head = countChangeRec(denominations.head, denominations)
+
+      //build sets with money - denominations.head money amount
+      val rest = countChangeRec(money-denominations.head, denominations)
+
+      //merge sets and remove duplicates
+
+      val merge = for {
+        lead <- head
+        rear <- rest
+      } yield (lead ++ rear).sorted
+
+      //return merged sets
+      return merge
+
+    } else { // (money < denominations.head) {
+      return countChangeRec(money, denominations.tail)
+    }
+
+    return Set()
+
+  }
+
   def countChange(money: Int, coins: List[Int]): Int = {
 
-    for {
-      coin <- coins
-    } yield countChange(money, List(coin))
+
+    val sets = countChangeRec(money, coins.sorted.reverse)
+
+    if (!sets.isEmpty){
+
+      val items = for {
+        list <- sets
+      } yield list.sum
+
+      return sets.size
+    }
+
+    return 0
   }
+
 
 
 
--- recfun/src/main/scala/recfun/tester.sc
+++ recfun/src/main/scala/recfun/tester.sc
@@ -1,4 +1,4 @@
 import recfun.Main._
 object tester {
-  val sums = countChange(2, List(1, 2))
+  val sums = countChange(300,List(5,10,20,50,100,200,500))
 }
