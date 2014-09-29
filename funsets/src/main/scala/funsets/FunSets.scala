67a99da5f8629980ade6010dea0df1379fd8a482
jfarid27 <jrab227@gmail.com>
2014-09-29 19:02:23 -0400

    Added funsets implementations for hw 2.

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -20,30 +20,42 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set =
+    ( (x: Int) => if (x == elem) true else false)
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set =
+    ((x: Int) =>
+      if ( contains(s, x) || contains(t, x) ) true
+      else false)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set =
+    ((x: Int) =>
+      if ( contains(s, x) && contains(t, x) ) true
+      else false)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = ((x: Int) =>
+    if ( contains(s, x) && !contains(t, x) ) true
+    else false)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set =
+    ((x: Int) =>
+      if ( contains(s, x) && p(x) ) true
+      else false)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -54,24 +66,41 @@ object FunSets {
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
-    }
-    iter(???)
+     if (exists(s, ((x:Int) => !p(x))) ) false else true
   }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    def iter(a: Int): Boolean = {
+      if (contains(s, a) && p(a)) true
+      else if (a < bound) iter(a+1)
+      else false
+    }
+    iter(-bound)
+  }
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = {
+    def iter(a: Int): Set = {
+
+
+      if (a > bound) {
+        filter(singletonSet(1), ((x:Int) => x != 1))
+      }
+      else if (contains(s, a)) {
+        union(singletonSet(f(a)), iter(a+1)   )
+      }
+      else iter(a+1)
+    }
+
+
+    iter(-bound)
+  }
 
   /**
    * Displays the contents of a set
